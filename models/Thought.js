9c4a8b83694c83e7a6598218b734a065791add67
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-09 17:55:57 -0600

    update, completed thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,27 +1,57 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
 
-const assignmentSchema = new Schema(
+const reactionSchema = new Reaction(
   {
-    assignmentId: {
+    reactionId: {
       type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
+      default: () => new Types.ObjectId,
     },
-    assignmentName: {
+    reactionBody: {
       type: String,
       required: true,
-      maxlength: 50,
-      minlength: 4,
-      default: 'Unnamed assignment',
+      minlength: 1,
+      maxlength: 255,
     },
-    score: {
-      type: Number,
+    username: {
+      type: String,
       required: true,
-      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+    createdAt: {
+      type: Date,
+      default: new Date.now,
+      get: createAtVal => moment(createAtVal).format('DD.MM.YYYY [at] HH:mm:ss'),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+)
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 255, xlength: 255,
     },
     createdAt: {
       type: Date,
       default: Date.now,
+      get: createAtVal => moment(createAtVal).format('DD.MM.YYYY [at] HH:mm:ss'),
     },
+    username: {
+      type: Number,
+      required: true,
+      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
@@ -31,4 +61,10 @@ const assignmentSchema = new Schema(
   }
 );
 
-module.exports = assignmentSchema;
+thoughtSchema.virtual('reactionCount').get(function(){
+  return this.reactions.length
+})
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
