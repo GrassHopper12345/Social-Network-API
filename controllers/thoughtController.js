f47591eb8d849c09aeeac08a749c8503e31fdeff
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-10 20:06:14 -0600

    completed thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -57,15 +57,23 @@ module.exports = {
     createReaction(req, res) {
         Thought.findOneAndUpdate(
             { _id: req.params.thoughtId },
-            { $addToSet: { reactions: req.body}},
+            { $addToSet: { reactions: req.body } },
             { runValidators: true, new: true }
         )
-        .then((thought) => !thought ?res.status(404).json({ messages: 'No thought found with that ID!'})
-        :res.json(thought)
-        )
-        .catch((error) => res.status(500).json(err));
+            .then((thought) => !thought ? res.status(404).json({ messages: 'No thought found with that ID!' })
+                : res.json(thought)
+            )
+            .catch((error) => res.status(500).json(err));
     },
     deleteReaction(req, res) {
-        
-    }
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: {reactionId: req.params.reactionId} } },
+            { runValidators: true, new: true }
+        )
+        .then((thought) => !thought ? res.status(404).json({message: 'No thought found with that ID!'})
+        :res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 };
