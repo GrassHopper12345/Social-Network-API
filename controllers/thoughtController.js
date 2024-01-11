09276fbcc1cf0dc9af9cb9a233a457e182581ada
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-10 19:10:20 -0600

    update to thoughtController, get single thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,7 +2,17 @@ const { User, Thought }  = require('../models');
 
 module.exports = {
  getThought(req, res) {
-
-}
+    Thought.find({})
+    .then((thought) => res.json(thought))
+    .catch((err) => res.status(500).json(err));
+},
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoguhtId})
+        .select("-__v").then((thoguht) => !thought ? res.status(404).json({message: "No thought found with that ID!"})
+        : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    
 
 }
